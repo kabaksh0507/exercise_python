6d3e7c690813b56060a399eda3ce9ca7ee44689e
kabaksh0507 <kabashmk0705@yahoo.co.jp>
2021-02-18 13:36:52 +0900

    Add 'fibonacci' module and test for exercise.

--- exercise_python/loadnumber.py
+++ exercise_python/loadnumber.py
@@ -13,6 +13,21 @@ def load_numbers_sorted(fname: str) -> List[int]:
         Return it as list.
     """
     numbers = []
+
     with open(fname) as inputfile:
         numbers = sorted(map(lambda e: int(e), inputfile))
+
     return numbers
+
+
+def fibonacci(number: int):
+    """
+    This function outputs a 'Fiboacci' sequence.
+      F0 = 0, F1 = 1
+      Fn+2 = Fn + Fn+1 (N ≥ 0)
+    """
+    sequence_pre = 0
+    sequence = 1
+    for _ in range(number):
+        print(sequence)
+        sequence_pre, sequence = sequence, sequence_pre + sequence
--- tests/test_loadnumber.py
+++ tests/test_loadnumber.py
@@ -5,7 +5,7 @@ This module tests the 'loadnumber' module.
 import os
 from typing import List
 import pytest
-from exercise_python.loadnumber import load_numbers_sorted
+from exercise_python.loadnumber import load_numbers_sorted, fibonacci
 
 
 @pytest.fixture
@@ -16,6 +16,7 @@ def txt() -> str:
     with open("numbers.txt", "w") as outfile:
         for num in [4, 5, 2, 3, 1]:
             outfile.write("{}\n".format(num))
+
     yield "numbers.txt"
     os.remove("numbers.txt")
 
@@ -30,6 +31,39 @@ def txt_and_list(txt) -> tuple[str, List[int]]:
 
 def test_load_numbers_sorted(txt_and_list):
     """
-    Test target module.
+    Test target module 'load_numbers_sorted'.
     """
     assert load_numbers_sorted(txt_and_list[0]) == txt_and_list[1]
+
+
+def test_fiboacci(capsys):
+    """
+    Test target module 'fiboacci'.
+    """
+    fibonacci(21)
+
+    out, _ = capsys.readouterr()
+
+    assert out == (
+        "1\n"
+        "1\n"
+        "2\n"
+        "3\n"
+        "5\n"
+        "8\n"
+        "13\n"
+        "21\n"
+        "34\n"
+        "55\n"
+        "89\n"
+        "144\n"
+        "233\n"
+        "377\n"
+        "610\n"
+        "987\n"
+        "1597\n"
+        "2584\n"
+        "4181\n"
+        "6765\n"
+        "10946\n"
+    )
